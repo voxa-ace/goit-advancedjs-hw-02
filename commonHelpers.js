615740b13e219da72ffe0164c7431436fd433b4d
voxa-ace <113594191+voxa-ace@users.noreply.github.com>
2024-06-13 16:39:21 +0000

    Deploying to gh-pages from @ voxa-ace/goit-advancedjs-hw-02@ca1fb8b336ac6cc661f3068da483599abf9e39a8 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";function o(){return`#${Math.floor(Math.random()*16777215).toString(16).padStart(6,"0")}`}const e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");let t;e.addEventListener("click",()=>{t||(e.disabled=!0,t=setInterval(()=>{document.body.style.backgroundColor=o()},1e3))});r.addEventListener("click",()=>{clearInterval(t),t=null,e.disabled=!1});
+//# sourceMappingURL=commonHelpers.js.map
